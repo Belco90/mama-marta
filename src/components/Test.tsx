53572bc14a5739ffb3ed02f47ecdd15e4c2b08ed
Mario Beltrán Alarcón <belco90@gmail.com>
2023-04-26 13:19:16 +0200

    Replace tailwind with chakra

---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Heading } from "@chakra-ui/react";
+
+interface TestProps {}
+
+const Test = (props: TestProps) => {
+  return <Heading fontSize="2xl">Chakra component</Heading>;
+};
+
+export default Test;
