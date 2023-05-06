12a8a4f5e0e805d37ab02508c4f8474b33a8b87f
Mario Beltrán Alarcón <belco90@gmail.com>
2023-05-06 17:32:12 +0200

    Apply better styles to details page

--- src/pages/recuerdos/[id]/index.tsx
+++ src/pages/recuerdos/[id]/index.tsx
@@ -7,14 +7,18 @@ import {
 	Center,
 	Heading,
 	HStack,
+	IconButton,
+	Show,
+	Spacer,
 	Stack,
 	Text,
 	useDisclosure,
 	useToast,
+	VStack,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { NextSeo } from 'next-seo'
-import { HiTrash } from 'react-icons/hi'
+import { HiTrash, HiPencil } from 'react-icons/hi'
 import useSWR from 'swr'
 
 import DeleteMemoryAlertDialog from '~/components/DeleteMemoryAlertDialog'
@@ -61,10 +65,65 @@ const DetailsMemoryPage = () => {
 		<>
 			<NextSeo title={memory.title} />
 			<>
-				<Heading variant="main">Un recuerdo...</Heading>
+				<HStack mb={4} spacing={0}>
+					<Heading variant="main">Un recuerdo...</Heading>
+					<Spacer />
+					<Show above="md">
+						<HStack>
+							<RouteLink
+								href={`/recuerdos/${memory.id}/editar`}
+								legacyBehavior
+								passHref
+							>
+								<Button
+									as="a"
+									colorScheme="secondary"
+									leftIcon={<HiPencil />}
+									size="xs"
+								>
+									Editar
+								</Button>
+							</RouteLink>
+							<Button
+								colorScheme="red"
+								onClick={onDeleteModalOpen}
+								leftIcon={<HiTrash />}
+								size="xs"
+							>
+								Borrar
+							</Button>
+						</HStack>
+					</Show>
+					<Show below="md">
+						<HStack spacing={1}>
+							<RouteLink
+								href={`/recuerdos/${memory.id}/editar`}
+								legacyBehavior
+								passHref
+							>
+								<IconButton
+									as="a"
+									aria-label="Editar"
+									icon={<HiPencil />}
+									colorScheme="secondary"
+									size="sm"
+									variant="outline"
+								/>
+							</RouteLink>
+							<IconButton
+								aria-label="Borrar"
+								icon={<HiTrash />}
+								colorScheme="red"
+								onClick={onDeleteModalOpen}
+								size="sm"
+								variant="outline"
+							/>
+						</HStack>
+					</Show>
+				</HStack>
 				<Stack
 					direction={{ base: 'column', md: 'row' }}
-					gap={{ base: 8, md: 60 }}
+					gap={{ base: 8, md: 10 }}
 					alignItems="start"
 				>
 					<Card
@@ -72,6 +131,7 @@ const DetailsMemoryPage = () => {
 						shadow="dark-lg"
 						borderRadius="0"
 						fontFamily="hand"
+						width={{ base: 'full', md: 'full' }}
 					>
 						<CardBody
 							aspectRatio="1/1"
@@ -80,32 +140,49 @@ const DetailsMemoryPage = () => {
 						>
 							<PictureImage memory={memory} />
 						</CardBody>
-						<CardHeader>
+						<CardHeader pt={0}>
 							<Center>
-								<Text as="h2" fontWeight="bold" fontSize="32" mb={8}>
+								<Text as="h2" fontWeight="bold" fontSize="3xl">
 									<Text as="u">{memory.title}</Text>
 								</Text>
 							</Center>
-							<Text fontSize={24} as="cite">
-								{String(new Date(memory.happenedAt))}
-								{!!memory.description && ` - ${memory.description}`}
-							</Text>
 						</CardHeader>
 					</Card>
 
-					<HStack gap={4}>
-						<Button
-							colorScheme="red"
-							onClick={onDeleteModalOpen}
-							leftIcon={<HiTrash />}
-							variant="link"
-						>
-							Borrar
-						</Button>
-						<RouteLink href={`/recuerdos/${memory.id}/editar`}>
-							Editar
-						</RouteLink>
-					</HStack>
+					<VStack width="full" alignItems="end">
+						{/* TODO: format happenedAt */}
+						<Text>Martes, 14 de abril de 2023</Text>
+
+						{!!memory.description && (
+							<Box
+								width={{ base: 'full', md: '80%' }}
+								shadow="2xl"
+								borderRadius="md"
+							>
+								<Box
+									width="full"
+									height="30px"
+									bgColor="#333"
+									borderTopRadius="inherit"
+								/>
+								<Text
+									fontFamily="hand"
+									fontSize="22px"
+									lineHeight="32px"
+									minHeight={{ base: '15vh', md: '20vh' }}
+									outline={0}
+									width="full"
+									height="full"
+									overflowY="auto"
+									background="repeating-linear-gradient(#F1EDE9, #F1EDE9 31px, #94ACD4 31px, #94ACD4 32px)"
+									px={4}
+									py={2}
+								>
+									{memory.description}
+								</Text>
+							</Box>
+						)}
+					</VStack>
 				</Stack>
 
 				{isDeleteModalOpen && (
